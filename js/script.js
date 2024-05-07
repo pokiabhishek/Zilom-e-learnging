5f0b7a4c55ef00ad21d4679216e8586dc6dddfd4
pokiabhishek <161606409+pokiabhishek@users.noreply.github.com>
2024-05-07 20:18:13 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,186 @@
+$(".slider").owlCarousel({
+  loop:true,
+  autoplay:true,
+  autoTimeout:2000,
+  
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
+
+
+$(".parent-faaturing").owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoTimeout:2000,
+  
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+
+
+$(".fixture").owlCarousel({
+    loop:true,
+ 
+    autoplay:true,
+    autoTimeout:2000,
+    dots:true,
+    
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+
+$(".parent-testomonial").owlCarousel({
+    loop:true, 
+    autoplay:true,
+    autoTimeout:2000,
+    dots:true,
+    
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+
+$(".logo-parent").owlCarousel({
+    loop:true, 
+    autoplay:true,
+    autoTimeout:2000,
+    dots:true,
+    
+    responsive: {
+      0: {
+        items: 2,
+      },
+      768: {
+        items: 4,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+
+  $(".Blog-slider").owlCarousel({
+    loop:true,
+  
+    autoplay:true,
+    autoTimeout:2000,
+    
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".main-course-card").owlCarousel({
+    loop:true,
+  
+    autoplay:true,
+    autoTimeout:2000,
+    
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 3,
+      },
+      992: {
+        items: 4,
+      },
+      1200: {
+        items: 5,
+      },
+    },
+  });
+
+  $(".first-card-11").owlCarousel({
+    loop:true,
+  
+    autoplay:true,
+    autoTimeout:2000,
+    
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+
+
+  // const optionmenu = document.querySelector(".select-menu"),
+  //       selectBtn = optionmenu.querySelector(".select-btn"),
+  //       options = optionmenu.querySelectorAll(".option"),
+  //       sBtn_text = optionmenu.querySelector(".sBtn-text");
+
+
+  // selectBtn.addEventListener("click", () => optionmenu.classList.toggle("active"));
+  
+  // options.forEach(option => {
+
+  //   option.addEventListener("click", () => {
+
+  //     let selectedOption = option.querySelector(".option-text").innerText;
+  //     // sBtn_text.innerText = selectedOption;
+  //     // console.log(selectedOption);
+
+  //   })
+  //   console.log(option);
+  // })
+
+
+
+
+
+
+
+
